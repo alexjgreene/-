414509e966c0e0f41766265bf3b263ef2ab817b2
alexjgreene <alex_greene@mail.ru>
2017-09-29 16:32:46 +0700

    Измененная полная табица умножения

--- index2.php
+++ index2.php
@@ -1,12 +1,40 @@
 		<div>
 		<?php
 		$z=1;
+		?>
+		<table>
+		<tr>
+		    <?php
 			for($x=1;$x<10;$x++){
+		    ?>		
+			<td>
+			<?php
+			echo "$x"
+			?>
+			</td>
+			<?php
+			}
+			?>
+		</tr>
+		<?php
+			for($x=1;$x<10;$x++){
+		?>
+			<tr>
+			    <?php
 				for($y=1;$y<10;$y++){
+				?>
+				<td>
+				<?php
 				$z=$x*$y;
 				echo "<p>$x*$y=$z</p>";
+				?>
+				</td>
+				<?php
 				}
-			echo "<br/>";
+				?>
+			</tr>
+			<?php
 			}
-		?>
+		  	?>
+		</table>
 		</div>
